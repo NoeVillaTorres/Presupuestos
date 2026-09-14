40ce1dee1a0b82745a611a7da842f0257d6471d0
NoeVillaTorres <noevdm2@hotmail.com>
2026-09-14 16:02:04 -0600

    postgress 2

--- build.sh
+++ build.sh
@@ -1,15 +1,7 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
 pip install -r requirements.txt
-
 python manage.py collectstatic --no-input
-
-# Si existe un disco de Render y no se ha copiado la base de datos todavía, copiarla:
-if [ -d "$DATA_DIR" ] && [ ! -f "$DATA_DIR/db.sqlite3" ]; then
-    echo "Copiando base de datos inicial al disco persistente..."
-    cp db.sqlite3 "$DATA_DIR/db.sqlite3"
-fi
-
 python manage.py migrate
+python manage.py loaddata datos_iniciales.json
